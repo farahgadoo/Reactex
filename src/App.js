b9c4f883b0d0acc42dbb5b4761ac12565125e011
farahgadoo <80074772+farahgadoo@users.noreply.github.com>
2021-11-18 20:40:11 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     	
+	<h1>Mahmoud Mohamed EL-Sabahy Mohamed </h1>
+	<h2>IS Department </h2>
+		
+    </div>
+  );
+}
+
+export default App;
